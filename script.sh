93bc603519ee7bbde7462dc42f1d45a7382eeb99
tito15951 <javierandres15951@gmail.com>
2021-12-12 17:02:28 -0500

    Reto 3 con script modificafo

--- script.sh
+++ script.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-intentos=3
+cd CodigoMPI/
 mpicc -Wall AgujasMPI.cc -o AgujasMPI -lm
 mpicc DardosMPI.cc -o DardosMPI
 g++ AgujasSecuencial.cc -o AgujasSecuencial
 g++ DardosSecuencial.cc -o DardosSecuencial
+mv DardosMPI ../
+mv AgujasMPI ../
+mv AgujasSecuencial ../
+mv DardosSecuencial ../
+cd
+scp DardosMPI cluser@wn1:/home/cluser
+scp DardosMPI cluser@wn2:/home/cluser
+scp DardosMPI cluser@wn3:/home/cluser
+scp AgujasMPI cluser@wn1:/home/cluser
+scp AgujasMPI cluser@wn2:/home/cluser
+scp AgujasMPI cluser@wn3:/home/cluser
 
 for j in 10 100 1000 1000 10000 100000
 do
-	for i in 1 ... 3
+	for i in 1 ... 5
 	do
 			echo "Agujas secuencial"
 			./AgujasSecuencial $j 1
